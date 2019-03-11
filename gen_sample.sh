455bd3c16dd8d7d641e1a6d144d03b2350c61383
angela <angelasnpang@gmail.com>
2019-03-11 12:13:46 +0000

    added our_problem and data

---
+++ gen_sample.sh
@@ -0,0 +1,11 @@
+USR_DIR=/content/prob
+PROBLEM=wiki_sql
+DATA_DIR=/content
+TMP_DIR=/content/temp_dir
+mkdir -p $TMP_DIR
+
+t2t-datagen \
+  --t2t_usr_dir=$USR_DIR \
+  --data_dir=$DATA_DIR \
+  --tmp_dir=$TMP_DIR \
+  --problem=$PROBLEM
